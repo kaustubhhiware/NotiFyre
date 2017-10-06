54e6279eb9f2c868531677da3a04bbefa07a66cb
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-10-06 18:36:19 +0530

    add debug version of the cleaned script

--- notifyre.sh
+++ notifyre.sh
@@ -6,16 +6,45 @@
 # Author: Kaustubh Hiware (hiwarekaustubh@googlemail.com)
 #
 
+SOUND=0
+ALERT=/usr/share/sounds/ubuntu/notifications/Slick.ogg
+MIN_INTERVAL=4
 
-notifyre(){
-    start=$(date +%s)
-    "$@"
-	ALERT=/usr/share/sounds/ubuntu/notifications/Slick.ogg
+notifyre() {
+  start=$(date +%s)
 
-	i="$IFS";IFS='/';set -f;p=($PWD);set +f;IFS="$i"; # echo "${p[-1]}"
-    [ $(($(date +%s) - start)) -le 0 ] || notify-send "Terminal in ${p[-2]}/${p[-1]} \$" "$(echo $@) completed in $(($(date +%s) - start)) seconds" -i utilities-terminal -t 50
-    # using gtk icons available
-	[ $(($(date +%s) - start)) -le 1 ] || paplay $ALERT
+  "$@"
+
+  i="$IFS";IFS='/'
+  set -f
+  p=($PWD)
+  set +f
+  IFS="$i"
+
+  end=$(date +%s)
+  elapsed=$((end - start))
+
+  if [ "$elapsed" -gt "$MIN_INTERVAL" ]; then
+    notify-send "Terminal in ${p[-2]}/${p[-1]} and exit $? \$" \
+      "completed $(echo $@) in $elapsed seconds" \
+      -i utilities-terminal -t 50
+
+    echo $SOUND
+
+    if [ "$SOUND" -eq 1 ]; then
+      if [ "$elapsed" -gt 1 ]; then
+        paplay $ALERT
+      else
+        echo "NOT PLAYING SOUND - elapsed < 1"
+      fi
+    else
+      echo "NOT PLAYING SOUND - configuration"
+    fi
+  else
+    echo "NOT NOTIFYING"
+  fi
+
+  # using gtk icons available
 
 }
 alias nf='notifyre'
@@ -23,16 +52,39 @@ alias nf='notifyre'
 # plain printer,
 notif_timer() {
 
-	ALERT=/usr/share/sounds/ubuntu/notifications/Slick.ogg
+  # check for new terminal condition, start is unitialised ? set it to current time
+  if [ -z $start ]; then
+    start=$(date +%s)
+  fi
 
-	# check for new terminal condition, start is unitialised ? set it to current time
-	if [ -z $start ]
-		then start=$(date +%s)
-	fi
-	i="$IFS";IFS='/';set -f;p=($PWD);set +f;IFS="$i"; # echo "${p[-1]}"
-    [ $(($(date +%s) - start)) -le 0 ] || notify-send "Terminal in ${p[-2]}/${p[-1]} \$" "$commandx completed in $(($(date +%s) - start)) seconds" -i ~/terminal.png -t 50
-	[ $(($(date +%s) - start)) -le 1 ] || paplay $ALERT
+  i="$IFS";IFS='/'
+  set -f
+  p=($PWD)
+  set +f
+  IFS="$i"
 
+  end=$(date +%s)
+  elapsed=$((end - start))
+
+  if [ "$elapsed" -gt "$MIN_INTERVAL" ]; then
+    notify-send "Terminal in ${p[-2]}/${p[-1]} and exit $? \$" \
+      "completed $(echo $@) in $elapsed seconds" \
+      -i utilities-terminal -t 50
+
+    echo $SOUND
+
+    if [ "$SOUND" -eq 1 ]; then
+      if [ "$elapsed" -gt 1 ]; then
+        paplay $ALERT
+      else
+        echo "NOT PLAYING SOUND - elapsed < 1"
+      fi
+    else
+      echo "NOT PLAYING SOUND - configuration"
+    fi
+  else
+    echo "NOT NOTIFYING"
+  fi
 }
 
 # needs .bash-preexec.sh
@@ -40,13 +92,10 @@ notif_timer() {
 # source ~/.bash-preexec.sh # as close to the end as posisble in bashrc
 
 preexec() {
-
-	commandx="$1"
-	start=$(date +%s)
-
+  commandx="$1"
+  start=$(date +%s)
 }
 
 precmd() {
-
-	notif_timer
+  notif_timer
 }
