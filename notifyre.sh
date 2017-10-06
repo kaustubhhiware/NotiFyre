789667ba3a87758e766415b61153ce621c6f2287
Siddharth Kannan <kannan.siddharth12@gmail.com>
2017-10-06 18:45:22 +0530

    remove all debug lines
    1. clean up code further, use a single function
    
    2. remove debug lines
    
    3. remove stray debug lines

--- notifyre.sh
+++ notifyre.sh
@@ -6,46 +6,17 @@
 # Author: Kaustubh Hiware (hiwarekaustubh@googlemail.com)
 #
 
-SOUND=0
+SOUND=1
+SOUND_MIN=10
 ALERT=/usr/share/sounds/ubuntu/notifications/Slick.ogg
 MIN_INTERVAL=4
 
 notifyre() {
   start=$(date +%s)
-
+  commandx=$@
   "$@"
 
-  i="$IFS";IFS='/'
-  set -f
-  p=($PWD)
-  set +f
-  IFS="$i"
-
-  end=$(date +%s)
-  elapsed=$((end - start))
-
-  if [ "$elapsed" -gt "$MIN_INTERVAL" ]; then
-    notify-send "Terminal in ${p[-2]}/${p[-1]} and exit $? \$" \
-      "completed $(echo $@) in $elapsed seconds" \
-      -i utilities-terminal -t 50
-
-    echo $SOUND
-
-    if [ "$SOUND" -eq 1 ]; then
-      if [ "$elapsed" -gt 1 ]; then
-        paplay $ALERT
-      else
-        echo "NOT PLAYING SOUND - elapsed < 1"
-      fi
-    else
-      echo "NOT PLAYING SOUND - configuration"
-    fi
-  else
-    echo "NOT NOTIFYING"
-  fi
-
-  # using gtk icons available
-
+  notif_timer
 }
 alias nf='notifyre'
 
@@ -68,22 +39,14 @@ notif_timer() {
 
   if [ "$elapsed" -gt "$MIN_INTERVAL" ]; then
     notify-send "Terminal in ${p[-2]}/${p[-1]} and exit $? \$" \
-      "completed $(echo $@) in $elapsed seconds" \
+      "completed $commandx in $elapsed seconds" \
       -i utilities-terminal -t 50
 
-    echo $SOUND
-
     if [ "$SOUND" -eq 1 ]; then
-      if [ "$elapsed" -gt 1 ]; then
+      if [ "$elapsed" -gt "$SOUND_MIN" ]; then
         paplay $ALERT
-      else
-        echo "NOT PLAYING SOUND - elapsed < 1"
       fi
-    else
-      echo "NOT PLAYING SOUND - configuration"
     fi
-  else
-    echo "NOT NOTIFYING"
   fi
 }
 
