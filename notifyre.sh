942ea52fb21d3d5ca8fe607390ed202b7aec4a0a
Athitya Kumar <athityakumar@gmail.com>
2017-10-12 10:40:56 +0530

    Adds status of previously executed command in notification (#9)
    * Adds status of previously executed command in notification
    
    * Adds ASCII tick and cross to messages

--- notifyre.sh
+++ notifyre.sh
@@ -37,19 +37,26 @@ notif_timer() {
   end=$(date +%s)
   elapsed=$((end - start))
 
+  if [[ "$RETVAL" -ne 0 ]]; then
+    command_status="\u274c"
+  else
+    command_status="\u2705"
+  fi
+
   if [ "$elapsed" -gt "$MIN_INTERVAL" ]; then
     if [ "$(uname -s)" = "Darwin" ]; then
       # Use terminal-notifier for Mac OS
-      terminal-notifier -title "NotiFyre"\
+      terminal-notifier -title "NotiFyre $(echo $command_status)"\
         -subtitle "Command : $commandx"\
         -message "Completed in $elapsed seconds"\
         -timeout 5\
         -closeLabel "Gotcha!"
     else
       # Use notify-send for others
-      notify-send "Terminal in ${p[-2]}/${p[-1]} and exit $? \$" \
+      notify-send "NotiFyre $(echo $command_status)" "Terminal in ${p[-2]}/${p[-1]} and exit $? \$" \
         "completed $commandx in $elapsed seconds" \
-        -i utilities-terminal -t 50
+        -i utilities-terminal
+        -t 50
     fi
 
     if [ "$SOUND" -eq 1 ]; then
